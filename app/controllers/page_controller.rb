578f981f7083b8efc6a00e49b06ebfa8d97ebc4d
Neonia <developer@neonia.org>
2015-03-23 18:34:01 -0600

    Join Neonia flow is working

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -7,19 +7,33 @@ class PageController < ApplicationController
     #render('index')
   end
   
-  def signup
-    @user = User.new
-    @user.name = params[:user][:name]
-    @user.photo = params[:user][:photo]
-    @user.subscribe = params[:user][:subscribe]
-    
+  def signup    
     # if user wants to subscribe to newsletter, then record email
-    #if params[:user][:subscribe] == 1
-      @user.email = params[:user][:email]
-    #else
-    #  @user.email = nil
-    #end
+    if params[:user][:subscribe] == "1"
+      
+      # do we already have a record with this email address?
+      # if so, then load that record and we'll just update the name and photo
+      if User.exists?(:email => params[:user][:email])
+        @user = User.find_by_email(params[:user][:email])
+        
+      # if not, then we'll create a new user
+      else
+        @user = User.new
+        @user.email = params[:user][:email]
+      end
+
+    # if user doesn't want to subscribe, then we'll
+    # not record their email    
+    else 
+      @user = User.new
+      @user.email = nil
+    end
     
+    # update all attributes
+    @user.require_photo = params[:user][:require_photo]
+    @user.subscribe = params[:user][:subscribe]
+    @user.name = params[:user][:name]
+    @user.photo = params[:user][:photo] 
     
     respond_to do |format|
       @user.save
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   # Field for e-mail signup
-  attr_accessor :subscribe, :boolean
+  attr_accessor :subscribe
+  # require_photo is a hidden field that causes photo validation
+  attr_accessor :require_photo
   
     # Photo
   has_attached_file :photo,
@@ -11,11 +13,14 @@ class User < ActiveRecord::Base
   :default_url => "/images/:style/missing.png"
   
   # Validations
-  validates_attachment_presence :photo
+  validates_attachment_presence :photo, unless: "require_photo.nil?"
   validates_attachment_content_type :photo, :content_type => /\Aimage/, :message => "needs to be an image file"
   validates :name, presence: true
   validates :email, presence: true, if: "subscribe == '1'"
-
+  validates :email, format: {
+    with: /.*@.*\..*/, 
+    message: "does not appear to be a correctly formatted address" }, if: "subscribe == '1'"
+  validates :email, uniqueness: true, if: "subscribe == '1'"
   
   
 end
