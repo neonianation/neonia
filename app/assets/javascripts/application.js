c732c268f2c302e7d3e5d04758612757dbe694a7
Neonia <developer@neonia.org>
2015-03-23 19:30:53 -0600

    Removed all traces of jquery-file-upload

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,13 +14,6 @@
 //= require bootstrap
 //= require jquery_ujs
 //= require jquery.remotipart
-//= require jquery-fileupload/vendor/jquery.ui.widget
-//= require jquery-fileupload/vendor/tmpl
-//= require jquery-fileupload/vendor/load-image.all.min
-//= require jquery-fileupload/vendor/canvas-to-blob
-//= require jquery-fileupload/jquery.iframe-transport
-//= require jquery-fileupload/jquery.fileupload
-//= require jquery-fileupload/jquery.fileupload-process
 //= require turbolinks
 //= require skrollr
 //= require skrollr.menu
