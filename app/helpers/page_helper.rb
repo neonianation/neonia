dc71517d2db49008f5fd4d0d0dce7dcfcbf53df3
Neonia <admin@neonia.org>
2015-04-02 13:20:23 +0000

    Added social media

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -1,2 +1,22 @@
 module PageHelper
+  
+  def social_bar
+
+    html = ""
+    
+    html += link_to "https://www.facebook.com/neonianation", :target => "_blank", :class => "social-icon" do
+      image_tag "facebook.png"
+    end
+    
+    html += link_to "https://plus.google.com/104547795772964098481/", :target => "_blank", :class => "social-icon" do
+      image_tag "googleplus.png"
+    end
+
+    html += link_to "https://www.twitter.com/neonianation", :target => "_blank", :class => "social-icon" do
+      image_tag "twitter.png"
+    end
+
+    return html
+    
+  end
 end
