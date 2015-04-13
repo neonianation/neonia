e6f4c615956705aeac2642977b3d5d3be1c4e916
Neonia <developer@neonia.org>
2015-04-13 00:42:07 -0600

    Prepared website for post-Thunderclap

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -4,18 +4,21 @@ module PageHelper
 
     html = ""
     
+    # FACEBOOK
     html += link_to "https://www.facebook.com/neonianation", :target => "_blank", :class => "social-icon" do
       image_tag "facebook.png"
     end
     
-    html += link_to "https://google.com/+NeoniaOrganization", :target => "_blank", :class => "social-icon", :rel => "publisher" do
-      image_tag "googleplus.png"
-    end
-
+    # TWITTER
     html += link_to "https://www.twitter.com/neonianation", :target => "_blank", :class => "social-icon" do
       image_tag "twitter.png"
     end
 
+    # GOOGLE+
+    html += link_to "https://google.com/+NeoniaOrganization", :target => "_blank", :class => "social-icon", :rel => "publisher" do
+      image_tag "googleplus.png"
+    end
+    
     return html
     
   end
